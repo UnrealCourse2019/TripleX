4a8907ac64b71ba8545a00bae4c5353bd3ea88ef
Ben Tristem <github@bentristem.com>
2019-02-28 14:27:20 +0000

    settings and early code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-#include <box2d>
 
 using namespace std;
 
-int main(int argc, char const *argv[])
+int main()
 {
-  cout << "Please enter two numbers" << endl;
+  std::cout << "You're a secret agent breaking into a server room." << endl;
+  std::cout << "Your SuperHacker 2000 tells you the following information" << endl;
+  std::cout << "+ There are three numbers in the code" << endl;
+
+  const int a = 3;
+  const int b = 4;
+  int sum = a + b;
+  int product = a * b;
+
+  std::cout << "+ The codes add-up to " << sum << endl;
+  std::cout << "+ The codes multiply to give " << product << endl;
+  
+  int answer[2];
+  while(std::cin >> answer)
+  {
+    std::cout << answer << endl;
+  }
 }
