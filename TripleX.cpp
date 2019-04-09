107c8004dfedf8a3be10263f55610179a7242396
Gavin Milroy <gavinmilroy@tesla-dev.com>
2019-04-09 14:41:02 +0100

    4_TX_UC2 Hello, World!

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,4 +1,9 @@
+#include <iostream>
+
 int main()
 {
+    std::cout << "You are a secret agent breaking into a secure server room...";
+    std::cout << std::endl;
+    std::cout << "Enter the correct code to continue...";
     return 0;
 }
