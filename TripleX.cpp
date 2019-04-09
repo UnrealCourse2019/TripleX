ef44325889f9cbed374ef57e003c6f151efbd5f4
Gavin Milroy <gavinmilroy@tesla-dev.com>
2019-04-09 18:02:57 +0100

    5_TX_UC2 Variables

--- TripleX.cpp
+++ TripleX.cpp
@@ -5,5 +5,17 @@ int main()
     std::cout << "You are a secret agent breaking into a secure server room...";
     std::cout << std::endl;
     std::cout << "Enter the correct code to continue...";
+
+    int a = 4;
+    int b = 3;
+    int c = 2;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
+
     return 0;
 }
