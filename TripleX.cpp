b22b641474338514593381cd991d0f2379235cea
Gavin Milroy <gavinmilroy@tesla-dev.com>
2019-04-10 21:41:56 +0100

    6_TX_UC2 Const & Assigning Values

--- TripleX.cpp
+++ TripleX.cpp
@@ -6,12 +6,14 @@ int main()
     std::cout << std::endl;
     std::cout << "Enter the correct code to continue...";
 
-    int a = 4;
-    int b = 3;
-    int c = 2;
 
-    int sum = a + b + c;
-    int product = a * b * c;
+    const int a = 4;
+    const int b = 3;
+    const int c = 2;
+
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
 
     std::cout << std::endl;
     std::cout << sum << std::endl;
