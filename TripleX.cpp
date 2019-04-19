cfa57b75ab74afedd6ac82254af47505da3a4379
Gavin Milroy <gavinmilroy@tesla-dev.com>
2019-04-19 18:31:27 +0100

    8 Naming & Self Documenting Code

--- TripleX.cpp
+++ TripleX.cpp
@@ -5,20 +5,23 @@ int main()
     // Print welcome messages to the terminal
     std::cout << "You are a secret agent breaking into a secure server room...";
     std::cout << std::endl;
-    std::cout << "Enter the correct code to continue...";
+    std::cout << "Enter the correct code to continue..." << std::endl;
 
-    // Declare 3 number code
-    const int a = 4;
-    const int b = 3;
-    const int c = 2;
+    const int CodeA = 4;
+    const int CodeB = 3;
+    const int CodeC = 2;
     
-    const int sum = a + b + c;
-    const int product = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
-    // Print sum and product to the terminal
+
+    // Print CodeSum and CodeProduct to the terminal
     std::cout << std::endl;
-    std::cout << sum << std::endl;
-    std::cout << product << std::endl;
+    std::cout << "+ There are 3 numbers in the code" << std::endl;
+    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
+    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
+
+    int PlayerGuess;
 
     return 0;
 }
