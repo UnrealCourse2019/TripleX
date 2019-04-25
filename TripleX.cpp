305607e4ab1af8b68f78f474d65e91575766b0ed
Gavin Milroy <gavinmilroy@tesla-dev.com>
2019-04-25 16:50:02 +0100

    11_TX_UC2 Functions

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
-int main()
+void PrintIntroduction()
 {
-    // Print welcome messages to the terminal
-    std::cout << "You are a secret agent breaking into a secure server room...";
-    std::cout << std::endl;
-    std::cout << "Enter the correct code to continue..." << std::endl;
+    std::cout << "You are a secret agent breaking into a secure server room...\n";
+    std::cout << "Enter the correct code to continue...\n\n";
+}
+
+void PlayGame()
+{
+    PrintIntroduction();
 
     const int CodeA = 4;
     const int CodeB = 3;
@@ -16,29 +19,30 @@ int main()
 
 
     // Print CodeSum and CodeProduct to the terminal
-    std::cout << std::endl;
-    std::cout << "+ There are 3 numbers in the code" << std::endl;
-    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
-    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
+    std::cout << "+ There are 3 numbers in the code";
+    std::cout << "\n+ The codes add-up to: " << CodeSum;
+    std::cout << "\n+ The codes multiply to give: " << CodeProduct << std::endl;
 
+    // Store player guess
     int GuessA, GuessB, GuessC;
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
+    // Check if the players guess is correct
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "You win!";
+        std::cout << "\nYou win!";
     }
-    
     else
     {
-        std::cout << "You lose!";
+        std::cout << "\nYou lose!";
     }
-    
+}
 
+int main()
+{
+    PlayGame();
     return 0;
 }
