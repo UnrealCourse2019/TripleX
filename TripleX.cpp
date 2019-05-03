c9ae85265c88dd78c6b2e73e84edb6df60acb3cc
Gavin Milroy <gavinmilroy@tesla-dev.com>
2019-05-03 16:34:24 +0100

    15_TX_UC2 Generating Random Number Ranges

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 
 void PrintIntroduction(int Difficulty)
 {
@@ -10,9 +11,10 @@ bool PlayGame(int Difficulty)
 {
     PrintIntroduction(Difficulty);
 
-    const int CodeA = rand();
-    const int CodeB = rand();
-    const int CodeC = rand();
+    // Generate Code
+    const int CodeA = rand() % Difficulty + Difficulty;
+    const int CodeB = rand() % Difficulty + Difficulty;
+    const int CodeC = rand() % Difficulty + Difficulty;
     
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
@@ -45,6 +47,8 @@ bool PlayGame(int Difficulty)
 
 int main()
 {
+    srand(time(NULL)); // create new random sequence based on time of day
+    
     int LevelDifficulty = 1;
     int const MaxDifficulty = 5;
 
@@ -58,7 +62,6 @@ int main()
         {
             ++LevelDifficulty;
         }
-        
     }
     std::cout << "\n*** Great work agent! You got all the files! Now get out of there! ***\n";
     return 0;
